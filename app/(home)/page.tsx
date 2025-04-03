61dd30f6b405d98eaeca6ba5f8c05646214b9b0c
Jonghwa <whdghk3471@gmail.com>
2025-04-03 12:30:05 +0900

    #3.4 Parallel Requests

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,17 +1,26 @@
 import { Metadata } from "next";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Home",
 };
 
-const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
+export const API_URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
 
 async function getMovies() {
-  await new Promise((resolve) => setTimeout(resolve, 5000));
-  return await (await fetch(URL)).json();
+  // await new Promise((resolve) => setTimeout(resolve, 1000));
+  return await (await fetch(API_URL)).json();
 }
 
 export default async function HomePage() {
   const movies = await getMovies();
-  return <div>{JSON.stringify(movies)}</div>;
+  return (
+    <div>
+      {movies.map((movie) => (
+        <li key={movie.id}>
+          <Link href={`/movies/${movie.id}`}>{movie.title}</Link>
+        </li>
+      ))}
+    </div>
+  );
 }
