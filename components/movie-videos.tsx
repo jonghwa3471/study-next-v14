cba8c246a758028f6054386874d95271679b0c1b
Jonghwa <whdghk3471@gmail.com>
2025-04-03 14:25:32 +0900

    #3.7 Error Handling

--- components/movie-videos.tsx
+++ components/movie-videos.tsx
@@ -2,9 +2,10 @@ import { API_URL } from "../app/(home)/page";
 
 async function getVideos(id: string) {
   console.log(`Fetching videos: ${Date.now()}`);
-  // await new Promise((resolve) => setTimeout(resolve, 3000));
-  const response = await (await fetch(`${API_URL}/${id}/videos`)).json();
-  return response;
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+  throw new Error("something broke...");
+  /* const response = await (await fetch(`${API_URL}/${id}/videos`)).json();
+  return response; */
 }
 
 export default async function MovieVideos({ id }: { id: string }) {
